a70e08be45177b1b0512f9ebc298cbf5e939decf
Serva <servas230@gmail.com>
2022-08-17 12:38:33 +0200

    Added delete and toggleCompleted functionality

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,27 +1,36 @@
 import { useSelector } from "react-redux/es/exports";
 import { selectTask } from "../store/tasks/selectors";
 import AddTaskForm from "./AddTaskForm";
+import { deleteTask, toggleCompleted } from "../store/tasks/slice";
+import { useDispatch } from "react-redux/es/exports";
+import { useState } from "react";
 
 const TaskList = () => {
+  const dispatch = useDispatch();
   const tasksData = useSelector(selectTask);
+  const [completed, setCompleted] = useState(false)
   console.log("list of tasks:", tasksData.tasks);
   return (
     <div>
       {" "}
       <p> Maximum amount of tasks: {tasksData.maxTasks}</p>
       <p> Show completed tasks: {tasksData.showCompletedTasks}</p>
+     
       {tasksData.tasks.map((task) => (
         <li>
           {" "}
-          {task.task}, Completed: {task.completed ? "yes" : "no"}
+          {task.task}, Completed: <button onClick={() => dispatch(toggleCompleted(task.id))}>{task.completed? "Yes" : "No"} </button>
+          <button onClick={() => dispatch(deleteTask(task.id))}> Delete the task</button>
         </li>
       ))}
       <div>
         {" "}
-        <AddTaskForm />
+       { tasksData.tasks.length < tasksData.maxTasks? <AddTaskForm /> : "No task can be added!" } 
       </div>{" "}
     </div>
   );
 };
 
 export default TaskList;
+
+ //tasksData.showCompletedTasks? ... : tasksData.task.filter(i => i.completed === false)
--- src/store/tasks/slice.js
+++ src/store/tasks/slice.js
@@ -1,28 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    maxTasks: 20,
-    tasks: [
-      { id: 1, task: "Pick up new glasses", completed: true },
-      { id: 2, task: "Buy airco", completed: false },
-      { id: 3, task: "Take packages to return", completed: false },
-      { id: 4, task: "Order dog food", completed: true }
-    ],
-    showCompletedTasks: false,
-  };
+  maxTasks: 20,
+  tasks: [
+    { id: 1, task: "Pick up new glasses", completed: true },
+    { id: 2, task: "Buy airco", completed: false },
+    { id: 3, task: "Take packages to return", completed: false },
+    { id: 4, task: "Order dog food", completed: true },
+  ],
+  showCompletedTasks: false,
+};
 
 export const taskSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {
     addTask: (state, action) => {
-       // console.log(action.payload)
-        const task = action.payload;
-        const newTask = {id:state.tasks.length, task, completed:false}
-state.tasks.push(newTask)
-    }
+      // console.log(action.payload)
+      const task = action.payload;
+      const newTask = { id: state.tasks.length + 1, task, completed: false };
+      state.tasks.push(newTask);
+      //state.tasks.length < state.maxTasks ? state.tasks.push(newTask) : state.tasks;
+    },
+    deleteTask: (state, action) => {
+      console.log("delete action", action.payload);
+      const id = action.payload;
+     const updatedList = state.tasks.filter((task) => task.id !== id);
+     state.tasks = updatedList;
+    },
+    toggleCompleted: (state, action) => {
+      // which one? => id
+      const taskId = action.payload;
+      const updatedTaskList = state.tasks.map((t) =>
+        t.id === taskId ? { ...t, completed: !t.completed } : t
+      );
+      state.tasks = updatedTaskList;
+    },
   },
 });
 
-export const {addTask} = taskSlice.actions;
+export const { addTask, deleteTask, toggleCompleted } = taskSlice.actions;
 export default taskSlice.reducer;
